9171f7cd71c753b0430182a7ce725288612c62b1
Natan Costa <natanscosta7@gmail.com>
2025-10-17 10:26:24 -0300

    base html and check if name is empty

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: index.html');
+    exit();
+}
+
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$confirmPassword = filter_input(INPUT_POST, 'confirm-password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+$errors = [];
+
+if ($name == '') {
+    $errors[] = "Nome precisa ser informado";
+}
+
+if (count($errors) > 0) {
+    echo "<h1>Bad Request</h1><ul>";
+    foreach ($errors as $e) {
+        echo "<li>$e</li>";
+    }
+    echo "</ul>";
+}
