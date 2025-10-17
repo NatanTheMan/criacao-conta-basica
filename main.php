f4192040eec526e922088321cb99f806e308efe1
Natan <natanscosta7@gmail.com>
2025-10-17 11:05:45 -0300

    require all inputs

--- main.php
+++ main.php
@@ -1,8 +1,8 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header('Location: index.html');
-    exit();
+  header('Location: index.html');
+  exit();
 }
 
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -13,13 +13,23 @@ $confirmPassword = filter_input(INPUT_POST, 'confirm-password', FILTER_SANITIZE_
 $errors = [];
 
 if ($name == '') {
-    $errors[] = "Nome precisa ser informado";
+  $errors[] = "Nome precisa ser informado";
+}
+if ($email == '') {
+  $errors[] = "Email precisa ser informado";
+}
+if ($password == '') {
+  $errors[] = "Senha precisa ser informado";
+}
+if ($confirmPassword == '') {
+  $errors[] = "Confime a senha";
 }
 
 if (count($errors) > 0) {
-    echo "<h1>Bad Request</h1><ul>";
-    foreach ($errors as $e) {
-        echo "<li>$e</li>";
-    }
-    echo "</ul>";
+  echo "<h1>Bad Request</h1><ul>";
+  foreach ($errors as $e) {
+    echo "<li>$e</li>";
+  }
+  echo "</ul>";
+  echo "<a href='index.html'>Voltar</a>";
 }
